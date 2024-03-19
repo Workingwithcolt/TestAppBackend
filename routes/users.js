e433fd9348dedd67477739ddb69cb7feb4ef5ae2
WorkingwithColt <mohitechetan2019@gmail.com>
2024-03-19 17:37:01 +0530

    Added the user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,55 @@
-import express from "express";
-var router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res) {
-  res.send({ message: "User " });
-});
+import { Router } from "express";
+import { asyncHandler } from "../helper/helper";
+const router = Router();
 
-export default router;
+router.get(
+  "/",
+  async function (req, res) {
+    let query = req.query ? req.query : {};
+    var usersController =
+      req.locals.controllerFactory.getUserController(req.locals)
+    var results =
+      await usersController.getAll(query);
+
+    res.send(results);
+  }
+);
+
+router.post('/', async (req, res) => {
+  console.log("print data");
+  console.log(req.body);
+  var AccountController =
+    req.locals.controllerFactory.getUserController(req.locals)
+  var results =
+    await AccountController.create(req.body)
+
+  res.send(results);
+})
+
+router.put("/",
+
+  async function (req, res) {
+    let query = req.query ? req.query : {};
+    var accountsController =
+      req.locals.controllerFactory.getUserController(req.locals);
+    let result = await accountsController.update(
+      query,
+      req.body
+    )
+    res.send(result)
+  });
+
+router.delete("/",
+
+  async function (req, res) {
+    let query = req.query ? req.query : {};
+    var accountsController =
+      req.locals.controllerFactory.getUserController(req.locals);
+
+    let result = await accountsController
+      .delete(query)
+    res.send(result)
+  });
+
+export default router
