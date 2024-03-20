346159b737026fd617bb209467ac160cc8955832
WorkingwithColt <mohitechetan2019@gmail.com>
2024-03-20 15:08:30 +0530

    code is added

--- routes/paymentRoute.cjs
+++ routes/paymentRoute.cjs
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router(); // Create a router using the function from the express module
-const stripe = require("stripe")("sk_test_51KAWSCSFbFeDhq0VqIOXaQQtkTfDQJowOzj5PvI5f2BtX5ZEUuNaeR5E5Dd7c0RYRIyyrqob9xNCLeKYN622TN7o007z49AxkP");
+const stripe = require("stripe")(process.env.StripeSecretKey);
 
 router.get('/',(req,res)=>{
   res.send({message:"success"})
@@ -27,7 +27,7 @@ console.log(res);
     paymentIntent: paymentIntent.client_secret,
     ephemeralKey: ephemeralKey.secret,
     customer: customer.id,
-    publishableKey: 'pk_test_51KAWSCSFbFeDhq0Vk731Cp1hOPQSBIYGgLdddjjA1ODAW990K51MDZ267GPaE8a04ZQ2rfU5PF1HX3qaSQ2q96VI00qnwJ2bd8'
+    publishableKey: process.env.pubKeyStripe
   });
 })
 
