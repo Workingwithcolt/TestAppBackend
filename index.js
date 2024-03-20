851b406e03cbb6c38c210e41ee8c8e4834622df4
WorkingwithColt <mohitechetan2019@gmail.com>
2024-03-20 14:13:14 +0530

    added payment stripe routee

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import indexRouter from "./routes/index.js"
 import userRouter from "./routes/users.js"
 import signupRouter from "./routes/signup.js"
 import signinRouter from "./routes/signin.js"
+import paymentRouter from "./routes/paymentRoute.js"
 import { mongodbMiddleware } from './MongoDB/Mongodbmiddleware.js'
 import bodyParser from "body-parser";
 var app = express();
@@ -29,6 +30,8 @@ app.use("/signup", signupRouter)
 app.use("/signin", signinRouter)
 
 app.use("/users", userRouter)
+
+app.use('/payments',paymentRouter)
 
 app.listen(3000, () => {
     console.log("server is running");
